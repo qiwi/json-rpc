629b5c5df52e7e3d26a86d5980e5e2c40ad6c9a2
Anton Golub <mailbox@antongolub.ru>
2019-10-14 14:27:21 +0300

    chore(common): expose IParsedObject iface

--- packages/common/src/main/ts/protocol.ts
+++ packages/common/src/main/ts/protocol.ts
@@ -1,6 +1,16 @@
-import {parse} from 'jsonrpc-lite'
+import {
+  parse,
+  IParsedObjectSuccess,
+  IParsedObjectNotification,
+  IParsedObjectRequest,
+  IParsedObjectError,
+  IParsedObjectInvalid
+} from 'jsonrpc-lite'
 
 export * from 'jsonrpc-lite'
 
 // TODO refactor after merge https://github.com/teambition/jsonrpc-lite/pull/14
 export const parseJsonRpcObject = (body: any) => parse(JSON.stringify(body))
+
+export type IParsedObject = IParsedObjectSuccess | IParsedObjectNotification |
+  IParsedObjectRequest | IParsedObjectError| IParsedObjectInvalid
